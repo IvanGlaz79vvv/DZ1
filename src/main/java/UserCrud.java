f2deb9ab07ab40003bfb74433fb3e0c8e0c8b53a
Иван <ivangl79@gmail.com>
2024-10-23 12:16:51 +0300

    добавил в UsersCrud всякого, можно реальизовать методы в main и уже что-то работает

--- src/main/java/UserCrud.java
+++ src/main/java/UserCrud.java
@@ -1,5 +1,6 @@
 import java.util.List;
 
+
 public class UserCrud implements Crud<UserCrud> {
     private int id;
     private String name;
@@ -51,6 +52,15 @@ public class UserCrud implements Crud<UserCrud> {
     public int getId() {
         return id;
     }
+    public String getName() {return name;}
+    public String getSoname() {return soname;}
+    public String getEmail() {return email;}
+    public String getPassword() {return password;}
+    public void setId(int id) {this.id = id;}
+    public void setName(String name) {this.name = name;}
+    public void setSoname(String soname) {this.soname = soname;}
+    public void setEmail(String email) {this.email = email;}
+    public void setPassword(String password) {this.password = password;}
 
     @Override
     public String toString() {
